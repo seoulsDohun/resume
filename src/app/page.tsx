3e1f2b1b77b559ab6e25e3c642ca6692035086cb
dohun <gkgk586@gmail.com>
2024-10-22 20:25:11 +0900

    feat: 이력서 스크롤 애니메이션 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,75 @@
+"use client";
+
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
 import About from './components/About';
-import Contact from './components/Contact';
 import Experience from './components/Experience';
 import OtherExperiences from './components/OtherExperiences';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 export default function Home() {
+  const aboutRef = useRef(null);
+  const experienceRef = useRef(null);
+  const otherExperiencesRef = useRef(null);
+  const skillsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.3,
+    };
+
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    const sections = [
+      aboutRef.current,
+      experienceRef.current,
+      otherExperiencesRef.current,
+      skillsRef.current,
+      contactRef.current,
+    ];
+
+    sections.forEach((section) => {
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        if (section) observer.unobserve(section);
+      });
+    };
+  }, []);
+
   return (
-    <>
-      <Head>
-        <title>김도훈의 기술이력서</title>
-        <meta name="description" content="프론트엔드 개발자 김도훈의 기술이력서 입니다." />
-        <meta name="keywords" content="프론트엔드, 개발자, 김도훈, 기술이력서" />
-        <meta property="og:title" content="김도훈의 기술이력서" />
-        <meta
-          property="og:description"
-          content="프론트엔드 개발자 김도훈의 기술이력서 입니다."
-        />
-        <meta property="og:type" content="website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
+    <main>
+      <div ref={aboutRef} className="section">
         <About />
+      </div>
+      <div ref={experienceRef} className="section">
         <Experience />
+      </div>
+      <div ref={otherExperiencesRef} className="section">
         <OtherExperiences />
+      </div>
+      <div ref={skillsRef} className="section">
         <Skills />
+      </div>
+      <div ref={contactRef} className="section">
         <Contact />
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
